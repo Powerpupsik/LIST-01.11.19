7aaec7ecc234f0be8b54370d27103150bd9f1ae2
Opilane TTHK <opilane@tthk.ee>
2019-11-01 18:18:06 +0200

    list

--- DateTimeConsolApp/Program.cs
+++ DateTimeConsolApp/Program.cs
@@ -10,6 +10,58 @@ namespace DateTimeConsolApp
     {
         static void Main(string[] args)
         {
+            /*var dateToday = new DateTime(1988, 12, 9);
+            Console.WriteLine(dateToday);
+            var dayOfTheWeek = dateToday.DayOfWeek;
+            Console.WriteLine(dayOfTheWeek);*/
+
+
+            //Küsib sünnikuud ja päeva ning ütleb, mis päeval sa sündisid.
+            /*Console.WriteLine("Enter your birth year, month and date: ");
+            string birthYear = Console.ReadLine();
+            GetArrayFromString(birthYear);*/
+
+            /*string userYear;
+            Console.WriteLine("Enter your date of birth(yyyy/m/dd)")
+            userYear= Console.ReadLine();
+
+            string[] userData = userYear.Split('/');
+            int year = int.Parse(userData[0]); 
+            int month= int.Parse(userData[1]);
+            int day= int.Parse(userData[2]);
+
+            var userBirthday = new DateTime(year, month, day);
+            var birthdayDayOfTheWeek = userBirthday.DayOfWeek;
+            Console.WriteLine($"You were born on {birthdayDayOfTheWeek}");
+
+            Console.ReadLine();*/
+
+
+            //KUUPÄEV TÄNA
+
+            var currentDate =  DateTime.Now;
+            var dateObject = new DateTime(2019, 11, 1);
+            Console.WriteLine(currentDate);
+           
+
+            var currentDayOfTheWeek = currentDate.DayOfWeek; //var on objekt; ükskõik mida salvestad sinna
+           
+
+            var ticks = dateObject.Ticks;
+            Console.WriteLine($"Ticks: {ticks}");
+
+            Console.WriteLine($"Print current date: {currentDate}");
+            Console.WriteLine($"Print long date string: {currentDate.ToLongDateString()}");
+            Console.WriteLine($"Print long date string: {currentDate.ToShortDateString()}");
+            Console.WriteLine($"Print long time to string: {currentDate.ToLongTimeString()}");
+            Console.WriteLine($"Print long time to string: {currentDate.ToShortTimeString()}");
+            Console.WriteLine($"Date format: {currentDate.ToString("dd-MM-yyyy HH:mm")}");
+
+
+            Console.ReadLine();
+
         }
+       
+        
     }
 }
---
+++ MyList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>() { 1, 2, 3, 4, 5 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 6, 7, 8}); //et lisada mitu numbrit
+           
+
+            foreach(int element in numbers)
+            {
+                Console.WriteLine(element);
+            }
+
+            int index = numbers.IndexOf(1);
+            Console.WriteLine($"Value of IndexOf: {index}");
+            int lastIndex = numbers.LastIndexOf(1);
+            Console.WriteLine($"Value of LastIndexOf: {lastIndex}");
+            //numbers.RemoveAt(lastIndex); //mingi elemendi eemaldamine
+
+            
+
+            int sizeOfList = numbers.Count;
+            Console.WriteLine($"Your list is {sizeOfList} long");
+
+            for (int i=0; i< numbers.Count; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    numbers.Remove(numbers[i]);
+                }
+            }
+
+            numbers.Clear();
+
+            foreach (int element in numbers)
+            {
+                Console.WriteLine($"List after Clear() {element}");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
